19d65d05527a521ee6ce96888c3e7c6d23d9dcc4
Atanassov <jj.websolutions@proton.me>
2024-05-01 20:10:49 +0200

    upload fix

--- src/components/AddMichiForm.tsx
+++ src/components/AddMichiForm.tsx
@@ -1,23 +1,67 @@
 'use client'
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import { createBrowserClient } from '@/utils/supabase'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { Card, CardContent } from './ui/card'
-import { Loader2 } from 'lucide-react'
-import { UploadButton } from '@/utils/uploadthing'
-import { UploadDropzone } from '@/utils/uploadthing'
+import { Loader2, Upload } from 'lucide-react'
+import { UploadButton, UploadDropzone } from '@/utils/uploadthing'
+import { useUploadThing } from '@/utils/uploadthing'
+import { on } from 'events'
+
 const AddMichiForm = () => {
   const supabase = createBrowserClient()
   const [title, setTitle] = useState('')
   const [tags, setTags] = useState('')
-  const [file, setFile] = useState<File | null>(null)
+  const [file, setFile] = useState(null)
+  const [previewUrl, setPreviewUrl] = useState('')
+  const [uploading, setUploading] = useState(false)
+
+  // Handle file selection for preview
+  const onDrop = useCallback((acceptedFiles: any) => {
+    setFile(acceptedFiles[0])
+    setPreviewUrl(URL.createObjectURL(acceptedFiles[0]))
+  }, [])
+
+  // Configure the upload logic
+  const { startUpload, permittedFileInfo } = useUploadThing('imageUploader', {
+    onBeforeUploadBegin(files) {
+      if (title === '' || tags === '') {
+        alert('Please fill in the title and tags')
+        return []
+      }
+      return files
+    },
+    onClientUploadComplete: () => {
+      alert('Uploaded successfully!')
+      setTags('')
+      setTitle('')
+      setFile(null)
+      setPreviewUrl('')
+      setUploading(false)
+    },
+    onUploadError: () => {
+      alert('Error occurred while uploading')
+      setUploading(false)
+    },
+    onUploadBegin: () => {
+      setUploading(true)
+    },
+  })
+
+  const fileTypes = permittedFileInfo?.config
+    ? Object.keys(permittedFileInfo?.config)
+    : []
+
+  useEffect(() => {
+    console.log('previewUrl', previewUrl)
+  }, [previewUrl])
 
   return (
     <Card className="min-w-full">
       <CardContent className="space-y-5 p-5">
         <h2 className="text-xl font-bold">Add Michi</h2>
-        <div className="flex flex-col space-y-4 ">
+        <div className="flex flex-col space-y-4">
           <Input
             type="text"
             placeholder="Title"
@@ -32,18 +76,27 @@ const AddMichiForm = () => {
             onChange={(e) => setTags(e.target.value)}
             required
           />
+          {previewUrl && (
+            <img
+              src={previewUrl}
+              alt="Preview"
+              className="h-32 w-32 object-cover"
+            />
+          )}
+
           <UploadDropzone
-            className="mt-4 ut-button:bg-red-500 ut-button:ut-readying:bg-red-500/50"
+            className="mt-4"
             endpoint="imageUploader"
-            onBeforeUploadBegin={(files) => {
+            onBeforeUploadBegin={(files: any) => {
               if (title === '' || tags === '') {
                 alert('Please fill in the title and tags')
+                setFile(null)
+                setPreviewUrl('')
                 return []
               }
-              // Preprocess files before uploading (e.g. rename them)
-              return files.map(
-                (f) => new File([f], 'renamed-' + f.name, { type: f.type }),
-              )
+              setPreviewUrl(URL.createObjectURL(files[0]))
+              onDrop(files)
+              return files
             }}
             onClientUploadComplete={async (res) => {
               const { error } = await supabase
@@ -53,17 +106,8 @@ const AddMichiForm = () => {
                 alert('Error inserting image data')
                 return
               }
-              // Do something with the response
-              console.log('Files: ', res)
-              res.forEach((file) => {
-                console.log('File URL:', file.url)
-              })
-              setTags('')
-              setTitle('')
-              alert('Upload Completed')
             }}
-            onUploadError={(error: Error) => {
-              // Do something with the error.
+            onUploadError={(error) => {
               alert(`ERROR! ${error.message}`)
             }}
           />
--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -2,7 +2,10 @@ import {
   generateUploadButton,
   generateUploadDropzone,
 } from '@uploadthing/react'
-
+import { generateReactHelpers } from '@uploadthing/react'
 import { OurFileRouter } from '@/app/api/uploadthing/core'
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>()
 export const UploadButton = generateUploadButton<OurFileRouter>()
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
