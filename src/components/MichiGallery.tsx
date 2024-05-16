204b3b929869571a4119202b65c674af8f57f1ee
Atanassov <jj.websolutions@proton.me>
2024-05-16 21:18:13 +0200

    Update MichiGallery.tsx

--- src/components/MichiGallery.tsx
+++ src/components/MichiGallery.tsx
@@ -45,16 +45,14 @@ function MichiGallery({ michis }: MichisGalleryProps) {
         onChange={(e) => setSearch(e.target.value)}
         className="mb-4 w-1/2 rounded-lg border p-2"
       />
-      <div className="grid grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-5">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-5">
         {filteredMichis.map((michi, index) => (
           <div key={michi.url + index} className="group relative">
-            <div
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={handleMouseLeave}
-              className={`block cursor-pointer sm:hidden`}
-              onClick={() =>
-                setActiveIndex(index === activeIndex ? null : index)
-              }
+            <a
+              href={michi.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block sm:hidden"
             >
               <Image
                 width={200}
@@ -69,9 +67,9 @@ function MichiGallery({ michis }: MichisGalleryProps) {
                 }`}
               >
                 <p className="text-lg font-bold">{michi.title}</p>
-                <p className="text-sm">{michi.tags}</p>
+                {/* <p className="text-sm">{michi.tags}</p> */}
               </div>
-            </div>
+            </a>
             <a
               href={michi.url}
               target="_blank"
@@ -87,7 +85,7 @@ function MichiGallery({ michis }: MichisGalleryProps) {
               />
               <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 px-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                 <p className="text-lg font-bold">{michi.title}</p>
-                <p className="text-sm">{michi.tags}</p>
+                {/* <p className="text-sm">{michi.tags}</p> */}
               </div>
             </a>
           </div>
