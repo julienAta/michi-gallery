16cc29da5bc4f0a7e7b2d65a24e31f964021d312
Atanassov <jj.websolutions@proton.me>
2024-05-16 17:53:58 +0200

    Update Tokenomics.tsx

--- src/components/Tokenomics.tsx
+++ src/components/Tokenomics.tsx
@@ -5,7 +5,7 @@ function Tokenomics() {
   return (
     <div className=" py-12">
       <h1 className="mb-8 text-center text-4xl font-bold">Tokenomics</h1>
-      <div className="flex flex-wrap items-center justify-center md:space-x-8">
+      <div className="flex flex-wrap items-center justify-center md:space-x-8 ">
         <div className="w-full text-center md:w-1/3">
           <Image
             alt="LP Burned"
@@ -21,7 +21,7 @@ function Tokenomics() {
           <h2 className="text-xl font-semibold">Total supply</h2>
           <p className="text-lg text-slate-900">559,343,847</p>
         </div>
-        <div className="w-full text-center md:w-1/3">
+        <div className="w-full text-center md:w-1/6">
           <Image
             alt="LP Burned"
             className="mx-auto mb-4 rounded-full border-4 border-[#764824]"
